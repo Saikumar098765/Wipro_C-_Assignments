059e9bea9ea79d1187f247a9852199be876c4ab1
Saikumar098765 <saikumaruruvakili@gmail.com>
2024-07-10 17:12:28 +0530

    Add files via upload

---
+++ Day4/EvenandOddNumbers.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+int[] numbers = new int[5];
+int sumEven = 0;
+int sumOdd = 0;
+
+Console.WriteLine("Enter 20 numbers:");
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Number {i + 1}: ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+
+for (int i = 0; i < 5; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        sumEven += numbers[i];
+    }
+    else
+    {
+        sumOdd += numbers[i];
+    }
+}
+
+Console.WriteLine("Sum of even numbers:" +sumEven);
+Console.WriteLine("Sum of odd numbers:" +sumOdd);
