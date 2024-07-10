039c4a4f3c49b7c495ce6e583ec87bc422490842
Saikumar098765 <saikumaruruvakili@gmail.com>
2024-07-10 16:21:29 +0530

    Add files via upload

---
+++ Day4/IntegertoNumber.cs
@@ -0,0 +1,27 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int num = 754;
+        string output = NumToWords(num);
+        Console.WriteLine(output);
+        // Output: one two three
+    }
+    static string NumToWords(int num)
+    {
+        string[] digitToWord = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        string numStr = num.ToString();
+        string result = " ";
+
+        foreach (char digit in numStr)
+        {
+            int index = digit - '0';
+            // Convert char digit to int index
+            if (index >= 0 && index <= 9)
+            {
+                result += digitToWord[index] + " ";
+            }
+        }
+        return result.Trim();
+    }
+}
