d24cddffef2be6c3eab087b8f16d97ca08dfbac8
Saikumar098765 <saikumaruruvakili@gmail.com>
2024-07-10 15:24:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace forloop
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            //Print First 10Numbers
+            for (int j = 1; j <= 10; j++) 
+                {
+                    Console.Write(" "+j);
+                }
+            
+            Console.WriteLine("\n---------");
+
+            //Print Odd Numbers from 1 to 50
+            for(int j1 = 1; j1 <= 50; j1++)
+            {
+                j1 = j1 + 2;
+                j1++;
+                Console.Write(" " + j1);
+            }
+        }
+    }
+}
---
+++ UserLogin.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter Your UserID : ");
+string userid = Console.ReadLine();
+userid = userid.ToLower();
+
+Console.Write("Enter Your Password : ");
+string password = Console.ReadLine();
+
+if(userid.Trim() == "admin" && password.Trim() == "admin123")
+{
+    Console.WriteLine("Welcome to Home");
+
+}
+else
+{
+    Console.WriteLine("Get Out this Home");
+}
