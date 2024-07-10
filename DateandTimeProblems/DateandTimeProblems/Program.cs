94110f7d0907dc61135effdbd0cc22ca51cf9556
Saikumar098765 <saikumaruruvakili@gmail.com>
2024-07-10 11:36:07 +0530

    Add files via upload

---
+++ DateandTimeProblems/DateandTimeProblems/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace datetime
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime();
+            
+            //Todays Date
+            dt= DateTime.Now;
+            Console.WriteLine("Todays Date and Time : "+dt);
+            Console.WriteLine(dt.ToString("dddd-MMM-yyyy"));
+            
+            //Adding 10 days to Present Date
+            DateTime adddays = dt.AddDays(10);
+            Console.WriteLine("Adding 10Days Of Todays Date : "+adddays);
+            Console.WriteLine(adddays.ToString("ddd-MM-yyyy"));
+            
+            //Difference Between two dates
+
+            TimeSpan timeSpan = DateTime.Now - adddays;
+            int gapdays = timeSpan.Days;
+           
+
+            Console.WriteLine("Gap between 2 Dates : "+gapdays);
+            
+            DateTime ti = new DateTime();
+            ti = DateTime.Now;
+            Console.WriteLine(ti.ToString("ddd-dd-MMM-yyy"));
+        }
+    }
+}
