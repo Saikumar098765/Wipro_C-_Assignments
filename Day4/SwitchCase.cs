1ef0b4c9121b9fc1976a3a1a23616e1eb9773e25
Saikumar098765 <saikumaruruvakili@gmail.com>
2024-07-10 15:25:42 +0530

    Add files via upload

---
+++ Day4/SwitchCase.cs
@@ -0,0 +1,27 @@
+﻿const int SalesDept = 10;
+const int HrDept = 20;
+const int RrDept = 30;
+
+Console.WriteLine("Enter Dept Number : ");
+
+int dp = int.Parse(Console.ReadLine());
+
+switch (dp)
+    {
+        case SalesDept :
+        Console.WriteLine("SalesDept");
+        break;
+
+        case HrDept :
+        Console.WriteLine("HrDept");
+        break;
+
+        case RrDept :
+        Console.WriteLine("RrDept");
+        break;
+
+        default:
+        Console.WriteLine("NoDept");
+        break;
+        
+    }
