dc11440d920ad0f5b4b90551473a173700a28341
Saikumar098765 <saikumaruruvakili@gmail.com>
2024-07-10 15:27:07 +0530

    Add files via upload

---
+++ Day4/Multiple-IfElse-Dept.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin
+{
+    internal class Multiple_IfElse_Dept
+    {
+        const int hrDept = 10;
+        const int medicalDept = 20;
+        const int sedicalDept = 30;
+        const int travelDept = 50;
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Your Dept Name : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if(num == hrDept)
+            {
+                Console.WriteLine("Your HR Dept");
+            }
+            else if (num == medicalDept)
+            {
+                Console.WriteLine("Your Medical Dept");
+            }
+            else if (num == sedicalDept)
+            {
+                Console.WriteLine("Your Sedical Dept");
+            }
+            else if (num == travelDept)
+            {
+                Console.WriteLine("Your Travel Dept");
+            }
+            else
+            {
+                Console.WriteLine("Your Not in any Dept");
+            }
+
+
+        }
+    }
+}
---
+++ Day4/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter Your UserID : ");
+string userid = Console.ReadLine();
+userid = userid.ToLower();
+userid = userid.Trim();
+
+Console.Write("Enter Your Password : ");
+string password = Console.ReadLine();
+
+if(userid == "admin" && password == "admin123")
+{
+    Console.WriteLine("Welcome to Home");
+
+}
+else
+{
+    Console.WriteLine("Get Out this Home");
+}
