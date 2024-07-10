5746cb1f944a1337aaf2f92ad5a335bbe5c231c1
Saikumar098765 <saikumaruruvakili@gmail.com>
2024-07-10 18:30:33 +0530

    Add files via upload

---
+++ Day4/ProductPrice.cs
@@ -0,0 +1,16 @@
+﻿
+const int product = 5;
+decimal amount = 0;
+for (int i = 0; i < product; i++)
+{
+    Console.Write($"Enter Products Price {i+1} : ");
+    decimal cost = decimal.Parse(Console.ReadLine());
+    
+    Console.Write($"Enter Quality {i+1} : ");
+    int weight = int.Parse(Console.ReadLine());
+
+    amount += cost * weight;
+
+
+}
+Console.WriteLine("Total : " + amount);
