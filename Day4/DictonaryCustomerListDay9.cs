3a07a7015bf2a9e8eb999554a2beecaa67dec0e1
Saikumar098765 <saikumaruruvakili@gmail.com>
2024-07-17 15:39:12 +0530

    Add files via upload

---
+++ Day4/DictonaryCustomerListDay9.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CustomerList
+{
+    private Dictionary<string, List<string>> customers;
+
+    public CustomerList()
+    {
+        customers = new Dictionary<string, List<string>>();
+    }
+
+    public void AddCustomer(string name, string plan)
+    {
+        if (customers.ContainsKey(plan))
+        {
+            customers[plan].Add(name);
+        }
+        else
+        {
+            customers[plan] = new List<string> { name };
+        }
+    }
+
+    public void AddCustomerByPlan(string name, string plan)
+    {
+        
+        if (customers.ContainsKey(plan))
+        {
+            
+            customers[plan].Add(name);
+        }
+        else
+        {
+            
+            customers[plan] = new List<string> { name };
+        }
+    }
+
+    public void RemoveCustomer(string name)
+    {
+       
+        foreach (var plan in customers)
+        {
+           
+            if (plan.Value.Contains(name))
+            {
+                
+                plan.Value.Remove(name);
+
+                
+                if (plan.Value.Count == 0)
+                {
+                    customers.Remove(plan.Key);
+                }
+
+                
+                return;
+            }
+        }
+
+        
+        Console.WriteLine($"Customer {name} not found.");
+    }
+
+    public void PrintCustomers()
+    {
+        foreach (var plan in customers)
+        {
+            Console.WriteLine($"Plan: {plan.Key}");
+            foreach (var customer in plan.Value)
+            {
+                Console.WriteLine($"- {customer}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        CustomerList customerList = new CustomerList();
+
+       
+        customerList.AddCustomer("Sai", "Basic");
+        customerList.AddCustomer("Kumar", "Premium");
+        customerList.AddCustomer("Sai Kumar", "Gold");
+
+        
+        customerList.AddCustomerByPlan("Sai", "Enterprise");
+
+      
+        customerList.RemoveCustomer("Sai");
+
+       
+        customerList.PrintCustomers();
+    }
+}
